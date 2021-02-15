ea541d5e55ace9fd8512f900aeb16397b354ed97
root <kim.celtics@gmail.com>
2021-02-15 17:53:57 +0100

    Revert "second commit"
    This reverts commit 4f111331396e565c7a076d2589c3f010eec2b90d.

--- seqClass.py
+++ seqClass.py
@@ -20,4 +20,4 @@ if re.search('^[ACGTU]+$', args.seq):
     else:
         print ('The sequence can be DNA or RNA')
 else:
-    print ('The sequence is neither DNA or RNA')
+    print ('The sequence is not DNA nor RNA')

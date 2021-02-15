3ae075585ddf0586622807a5cdcc9862633ab4ce
root <kim.celtics@gmail.com>
2021-02-15 18:44:50 +0100

    task14 main

--- seqClass.py
+++ seqClass.py
@@ -26,6 +26,6 @@ if args.motif:
   args.motif = args.motif.upper()
   print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
   if re.match(args.motif, args.seq):
-    print("FOUND!!!! :D ")
+    print("FOUND1!!!! :D ")
   else:
     print("NOT FOUND :( ")
